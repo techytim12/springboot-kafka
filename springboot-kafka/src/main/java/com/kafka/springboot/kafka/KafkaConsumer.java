465d3bd77bbeae321642446ea26bf458ec9cd647
Thomas Philip <thomasphilipchry@gmail.com>
2024-03-18 06:17:20 +0530

    Refactored code

--- springboot-kafka/src/main/java/com/kafka/springboot/kafka/KafkaConsumer.java
+++ springboot-kafka/src/main/java/com/kafka/springboot/kafka/KafkaConsumer.java
@@ -2,6 +2,7 @@ package com.kafka.springboot.kafka;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -9,7 +10,7 @@ import org.springframework.stereotype.Service;
 public class KafkaConsumer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(KafkaConsumer.class);
-    @KafkaListener(topics = "topic-1", groupId = "myGroup")
+    @KafkaListener(topics = "${spring.kafka.topic.name}", groupId = "${spring.kafka.consumer.group-id}")
     public void consumer(String message){
         LOGGER.info(String.format("consumer Message Received -> %s", message));
     }
